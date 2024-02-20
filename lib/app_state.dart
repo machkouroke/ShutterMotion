dbd7506faa17c78209311d9ff5be7cf56f446ab7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-20 21:28:02 +0000

    Updating to latest FlutterFlow output.

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:csv/csv.dart';
+import 'package:synchronized/synchronized.dart';
+import 'flutter_flow/flutter_flow_util.dart';
 
 class FFAppState extends ChangeNotifier {
   static FFAppState _instance = FFAppState._internal();
@@ -15,17 +18,14 @@ class FFAppState extends ChangeNotifier {
   }
 
   Future initializePersistedState() async {
-    prefs = await SharedPreferences.getInstance();
-    _safeInit(() {
-      _xAxis =
-          prefs.getStringList('ff_xAxis')?.map(double.parse).toList() ?? _xAxis;
+    secureStorage = const FlutterSecureStorage();
+    await _safeInitAsync(() async {
+      _tempSeuil = await secureStorage.getDouble('ff_tempSeuil') ?? _tempSeuil;
     });
-    _safeInit(() {
-      _yAxis =
-          prefs.getStringList('ff_yAxis')?.map(double.parse).toList() ?? _yAxis;
-    });
-    _safeInit(() {
-      _tempSeuil = prefs.getDouble('ff_tempSeuil') ?? _tempSeuil;
+    await _safeInitAsync(() async {
+      _location =
+          latLngFromString(await secureStorage.getString('ff_location')) ??
+              _location;
     });
   }
 
@@ -34,83 +34,30 @@ class FFAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  late SharedPreferences prefs;
-
-  List<double> _xAxis = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
-  List<double> get xAxis => _xAxis;
-  set xAxis(List<double> value) {
-    _xAxis = value;
-    prefs.setStringList('ff_xAxis', value.map((x) => x.toString()).toList());
-  }
-
-  void addToXAxis(double value) {
-    _xAxis.add(value);
-    prefs.setStringList('ff_xAxis', _xAxis.map((x) => x.toString()).toList());
-  }
-
-  void removeFromXAxis(double value) {
-    _xAxis.remove(value);
-    prefs.setStringList('ff_xAxis', _xAxis.map((x) => x.toString()).toList());
-  }
-
-  void removeAtIndexFromXAxis(int index) {
-    _xAxis.removeAt(index);
-    prefs.setStringList('ff_xAxis', _xAxis.map((x) => x.toString()).toList());
-  }
-
-  void updateXAxisAtIndex(
-    int index,
-    double Function(double) updateFn,
-  ) {
-    _xAxis[index] = updateFn(_xAxis[index]);
-    prefs.setStringList('ff_xAxis', _xAxis.map((x) => x.toString()).toList());
-  }
-
-  void insertAtIndexInXAxis(int index, double value) {
-    _xAxis.insert(index, value);
-    prefs.setStringList('ff_xAxis', _xAxis.map((x) => x.toString()).toList());
-  }
-
-  List<double> _yAxis = [2.0, 3.0, 6.0, 4.0, 5.0, 3.0, 4.0];
-  List<double> get yAxis => _yAxis;
-  set yAxis(List<double> value) {
-    _yAxis = value;
-    prefs.setStringList('ff_yAxis', value.map((x) => x.toString()).toList());
-  }
-
-  void addToYAxis(double value) {
-    _yAxis.add(value);
-    prefs.setStringList('ff_yAxis', _yAxis.map((x) => x.toString()).toList());
-  }
-
-  void removeFromYAxis(double value) {
-    _yAxis.remove(value);
-    prefs.setStringList('ff_yAxis', _yAxis.map((x) => x.toString()).toList());
-  }
-
-  void removeAtIndexFromYAxis(int index) {
-    _yAxis.removeAt(index);
-    prefs.setStringList('ff_yAxis', _yAxis.map((x) => x.toString()).toList());
-  }
-
-  void updateYAxisAtIndex(
-    int index,
-    double Function(double) updateFn,
-  ) {
-    _yAxis[index] = updateFn(_yAxis[index]);
-    prefs.setStringList('ff_yAxis', _yAxis.map((x) => x.toString()).toList());
-  }
-
-  void insertAtIndexInYAxis(int index, double value) {
-    _yAxis.insert(index, value);
-    prefs.setStringList('ff_yAxis', _yAxis.map((x) => x.toString()).toList());
-  }
+  late FlutterSecureStorage secureStorage;
 
   double _tempSeuil = 20.0;
   double get tempSeuil => _tempSeuil;
   set tempSeuil(double value) {
     _tempSeuil = value;
-    prefs.setDouble('ff_tempSeuil', value);
+    secureStorage.setDouble('ff_tempSeuil', value);
+  }
+
+  void deleteTempSeuil() {
+    secureStorage.delete(key: 'ff_tempSeuil');
+  }
+
+  LatLng? _location = const LatLng(0, 0);
+  LatLng? get location => _location;
+  set location(LatLng? value) {
+    _location = value;
+    value != null
+        ? secureStorage.setString('ff_location', value.serialize())
+        : secureStorage.remove('ff_location');
+  }
+
+  void deleteLocation() {
+    secureStorage.delete(key: 'ff_location');
   }
 }
 
@@ -124,4 +71,47 @@ Future _safeInitAsync(Function() initializeField) async {
   try {
     await initializeField();
   } catch (_) {}
+}
+
+extension FlutterSecureStorageExtensions on FlutterSecureStorage {
+  static final _lock = Lock();
+
+  Future<void> writeSync({required String key, String? value}) async =>
+      await _lock.synchronized(() async {
+        await write(key: key, value: value);
+      });
+
+  void remove(String key) => delete(key: key);
+
+  Future<String?> getString(String key) async => await read(key: key);
+  Future<void> setString(String key, String value) async =>
+      await writeSync(key: key, value: value);
+
+  Future<bool?> getBool(String key) async => (await read(key: key)) == 'true';
+  Future<void> setBool(String key, bool value) async =>
+      await writeSync(key: key, value: value.toString());
+
+  Future<int?> getInt(String key) async =>
+      int.tryParse(await read(key: key) ?? '');
+  Future<void> setInt(String key, int value) async =>
+      await writeSync(key: key, value: value.toString());
+
+  Future<double?> getDouble(String key) async =>
+      double.tryParse(await read(key: key) ?? '');
+  Future<void> setDouble(String key, double value) async =>
+      await writeSync(key: key, value: value.toString());
+
+  Future<List<String>?> getStringList(String key) async =>
+      await read(key: key).then((result) {
+        if (result == null || result.isEmpty) {
+          return null;
+        }
+        return const CsvToListConverter()
+            .convert(result)
+            .first
+            .map((e) => e.toString())
+            .toList();
+      });
+  Future<void> setStringList(String key, List<String> value) async =>
+      await writeSync(key: key, value: const ListToCsvConverter().convert([value]));
 }
